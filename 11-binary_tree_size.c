3f58e2a4989637daa2e0ced95ba30deb28fff808
Noons-ledev <noah.briet@icloud.com>
2024-12-09 18:58:35 +0100

    T11 betty

--- 11-binary_tree_size.c
+++ 11-binary_tree_size.c
@@ -8,9 +8,10 @@
 size_t binary_tree_size(const binary_tree_t *tree)
 {
 	size_t left_side, right_side;
+
 	if (tree == NULL)
 	return (0);
 left_side = binary_tree_size(tree->left);
 right_side = binary_tree_size(tree->right);
-return ( 1 + left_side + right_side);
+return (1 + left_side + right_side);
 }
