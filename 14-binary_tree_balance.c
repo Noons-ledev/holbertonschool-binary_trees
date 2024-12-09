6af49f8e91b405b635a47e067d7fc5de0c282fad
Noons-ledev <noah.briet@icloud.com>
2024-12-09 21:58:26 +0100

    Getting balance with a height of child function

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,37 @@
+#include <stdlib.h>
+#include "binary_trees.h"
+/**
+*binary_tree_height- Return the height of a tree from a given node
+*@tree: Pointer to the root
+*Return: Height
+*/
+int binary_tree_height_child(const binary_tree_t *tree)
+{
+int left_height, right_height;
+
+	if (tree == NULL)
+	return (0);
+if (tree->left == NULL && tree->right == NULL)
+return (1);
+left_height = binary_tree_height_child(tree->left);
+right_height = binary_tree_height_child(tree->right);
+return (1 + (left_height > right_height ? left_height : right_height));
+}
+
+/**
+*binary_tree_balance-the balance(height diff btwn left/right subtrees)
+*@tree: Pointer to the root
+*Return: Difference
+*/
+int binary_tree_balance(const binary_tree_t *tree)
+{
+	int left_height, right_height;
+	if (tree == NULL)
+	return (0);
+	if (tree->left == NULL && tree->right == NULL)
+	return (0);
+	left_height = binary_tree_height_child(tree->left);
+	right_height = binary_tree_height_child(tree->right);
+	return (left_height - right_height);
+
+}
